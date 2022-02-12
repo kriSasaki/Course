7702ec13bda94e35101a6b53f2fec7663763c268
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-12 18:53:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEEBRAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int rowPictures = 3;
+            int rowCount = allPictures / rowPictures;
+            int excessCount = allPictures % rowCount;
+            Console.WriteLine("Заполненных рядов " + rowCount);
+            Console.WriteLine("Лишних картинок " + excessCount);
+        }
+    }
+}
