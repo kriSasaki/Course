fca34e56dc490531a3010681f88fab7380cafd8a
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-13 10:21:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int countMessages;
+            Console.Write("Введите сообщение: ");
+            message = Convert.ToString(Console.ReadLine());
+            Console.Write("Сколько раз отправить?: ");
+            countMessages =Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<countMessages; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
