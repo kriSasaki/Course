892e5d16b8d703b0b5ae513ff55d34ff0c641f43
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-13 11:40:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitProgram = "";
+            while (exitProgram != "exit")
+
+            {
+                Console.WriteLine("Программа выполняется, пока не поступит команда 'exit' ");
+                exitProgram = Console.ReadLine();
+                if (exitProgram != "exit")
+                {
+                    Console.WriteLine("Неизвестная команда");
+                }
+            }
+
+        }
+    }
+}
