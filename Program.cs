549674be96298d67108d6a0732c771b63a56acff
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-14 22:40:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequence = 0;
+            while (sequence< 98) //цикл проверяет условие, то есть пока он не дойдет до небходимого числа, не прекратит выводить числа.
+            {
+                sequence += 7;
+                Console.Write(sequence+" ");
+            }
+
+        }
+    }
+}
