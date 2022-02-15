b5b7727e78671c119a6094eee8234c4eb58b9a97
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-15 16:47:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 98;
+            int step = 7;
+
+            for (int sequence = 0; sequence < limit; ) // число итераций известно. While бесконечный цикл. For конечен. Я не понимаю((((
+            {
+                sequence += step;
+                Console.Write(sequence + " ");
+            }
+        }
+    }
+}
