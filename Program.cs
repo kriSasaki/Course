18f7712077cc51882e14acf451beb0589dd0e597
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-02-16 21:02:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characters = null;
+            int additionalSymbols = 2;
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();   
+            Console.Write("Введите символ: ");
+            string symbol = Console.ReadLine();
+            
+            for ( int i=1; i <= (name.Length + additionalSymbols + additionalSymbols); i += 1)
+            {
+                characters += symbol;
+            }
+            Console.WriteLine(characters + "" +
+                "\n" + symbol+" " + name +" "+ symbol +
+                "\n" + characters);
+        }
+    }
+}
