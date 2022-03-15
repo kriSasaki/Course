0ce6c4def3040ac870a06c20800b320e98737890
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-03-15 14:29:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "123";
+            string number = "qwe";
+            Console.WriteLine(name + " " + number);
+            string nameless = number;
+            number = name;
+            name = nameless;
+            Console.WriteLine(name + " " + number);
+        }
+    }
+}
