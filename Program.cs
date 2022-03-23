a2b216b2d7cceabaa8e82a86ba3b65cb25d20c1b
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-03-23 20:42:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minNumber = 1;
+            int maxNumber = 27;
+            int number = random.Next(minNumber, maxNumber);
+            int maxLimit = 999;
+            int minLimit = 100;
+            int amount = 0;
+
+            for(int i=1; i <= maxLimit; i += number)
+            {
+                if (i >= minLimit)
+                {
+                    amount += 1;
+                }
+            }
+            Console.WriteLine(amount);
+        }
+    }
+}
