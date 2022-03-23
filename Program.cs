ec2e9f69fcfe65c8a2260b54753054543192fa63
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-03-23 18:45:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxNumber = 100;
+            int number = random.Next(maxNumber);
+            int multiple = 3;
+            int sum = 0;
+
+            for (int i = 1; i <= number/multiple; i += 1)
+            {
+                sum += multiple;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
