59e6b01c65446832c4d43b4bfa4f7a4244713572
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-04-12 14:50:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[10];
+            int maxNumber=10;
+            int number=0;
+            int amount = 1;
+
+            for (int i =0;i<numbers.Length;i++)
+            {
+                numbers[i] = random.Next(maxNumber);
+                Console.Write(numbers[i] + " ");
+            }
+            for(int i = 0; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    number = numbers[i];
+                    amount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\n"+ number + "  " + amount);
+        }
+    }
+}
