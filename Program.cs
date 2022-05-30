a55e4475ac7926ad7a8c5183238eada4aec3c6e5
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-05-30 17:01:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumberconverted = false;
+
+            while (isNumberconverted == false)
+            {
+                Console.WriteLine("Введите строку: ");
+                string numberInString = Console.ReadLine();
+                
+                if (Int32.TryParse(numberInString, out int number))
+                {
+                    Console.WriteLine("Число: " + number);
+                    Console.ReadKey();
+                    isNumberconverted = true;
+                }
+                else
+                {
+                    Console.WriteLine("Нельзя сконфертировать");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
