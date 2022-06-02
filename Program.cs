9ed4d8a27b3c7da517615e24853df6544f42ec72
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-06-02 21:16:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_BraveNewWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            bool isPlaying = true;
+            int heroPositionX;
+            int heroPositionY;
+            int heroDirectionX = 0;
+            int heroDirectionY = 1;
+            int allDots = 0;
+            int collectedDots = 0;
+            
+            char[,] map = ReadMap(out heroPositionX, out heroPositionY, ref allDots);
+
+            DrawMap(map);
+
+            while (isPlaying)
+            {
+                Console.SetCursorPosition(0, 20);
+                Console.WriteLine($"Собранно ягод {collectedDots} из {allDots}");
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    ChangeDirection(key, ref heroDirectionX, ref heroDirectionY);
+
+                    if(map[heroPositionX + heroDirectionX, heroPositionY+ heroDirectionY] != '#')
+                    {
+                        Move(ref heroPositionX, ref heroPositionY, heroDirectionX, heroDirectionY);
+
+                        CollectDots(map, heroPositionX, heroPositionY, ref collectedDots);
+                    }
+                }
+                if (collectedDots == allDots)
+                {
+                    Console.Clear();
+                    isPlaying = false;
+                    Console.WriteLine("Конец");
+                }
+            }
+        }
+
+        static char[,] ReadMap(out int positionX, out int positionY, ref int allDots)
+        {
+            positionX = 0;
+            positionY = 0;
+
+            char[,] map =
+            {
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+                {'#', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', ' ', '#', '#', '#', '#', '*', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', '#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#', ' ', ' ', ' ', ' ', '#'},
+                {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '@', '#'},
+                {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            };
+
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == '@')
+                    {
+                        positionX = i;
+                        positionY = j;
+                    }
+                    else if (map[i, j] == '*')
+                    {
+                        allDots++;
+                    }
+                }
+            }
+            return map;
+        }
+
+        static void DrawMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void ChangeDirection(ConsoleKeyInfo key, ref int directionX, ref int directionY)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    directionX = -1;
+                    directionY = 0;
+                    break;
+                case ConsoleKey.DownArrow:
+                    directionX = 1;
+                    directionY = 0;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    directionX = 0;
+                    directionY = -1;
+                    break;
+                case ConsoleKey.RightArrow:
+                    directionX = 0;
+                    directionY = 1;
+                    break;
+            }
+        }
+
+        static void Move(ref int positionX, ref int positionY, int directionX, int directionY)
+        {
+            Console.SetCursorPosition(positionY, positionX);
+            Console.Write(" ");
+
+            positionX += directionX;
+            positionY += directionY;
+
+            Console.SetCursorPosition(positionY, positionX);
+            Console.Write('@');
+        }
+
+        static void CollectDots(char[,] map, int positionX, int positionY, ref int collectDots)
+        {
+            if (map[positionX, positionY] == '*')
+            {
+                collectDots++;
+                map[positionX, positionY] = ' ';
+            }
+        }
+
+    }
+}
