54e0aa4ce0747f9a2719864fbb5b81eb795d184b
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-06-03 15:18:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Shuffle(array);
+        }
+
+        static void Shuffle(int[] array)
+        {
+            Random random = new Random();
+            int randomElement;
+            int shuffledElement;
+
+            for(int i = array.Length - 1; i >= 0; i--)
+            {
+                randomElement = random.Next(i);
+                shuffledElement = array[randomElement];
+                array[randomElement] = array[i];
+                array[i] = shuffledElement;
+                Console.Write(array[i]);
+            }             
+        }
+    }
+}
