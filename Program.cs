8614e1a431c79afe5fff819ded84b1562efff76c
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-06-20 21:25:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_РаботаСКлассами
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player("Имя", 1000, 50);
+            player.ShowInfo();
+        }
+    }
+
+    class Player
+    {
+        private string _name;
+        private int _health;
+        private int _damage;
+
+        public Player(string name, int health, int damage)
+        {
+            _name = name;
+            _health = health;
+            _damage = damage;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"У игрока {_name} - {_health} хп и {_damage} урона");
+        }
+    }
+}
